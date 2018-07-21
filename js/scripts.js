ec77ff8701438e412e3c507e834a3ee1ae280bde
EugeneZnm <eugenenzioki@gmail.com>
2018-07-21 15:43:00 +0300

    addition of start game and die roll functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 //BUSINESS LOGIC
+
+// 3 Initialising objects as variables
 var playerone = "";
 var playertwo = "";
 
+// 4 generating numbers randomly
 var rolldice = function() {
     return Math.floor(6 * Math.random()) + 1;
 }
 
-//creating player constructor
+// 5 creating player constructor
 function player() {
     this.roll = 0;
     this.roundcore = 0;
@@ -14,13 +17,20 @@ function player() {
 }
 
 
-//USER INTERFACE
+// USER INTERFACE
+
+// 1 startgame click function
 $(document).ready(function() {
     $("button#startgame").click(function(event) {
         playerone = new player(true);
         playertwo = new player(false);
     });
 
+    // 2 Player One Roll dice click function
+    $("button#rollp1").click(function(event) {
+        playerone.roll = rolldice();
+        $("#rolldie").text(playerone.roll);
+    });
 
 
 });
